b4e46e29aabd19ade4976c16a0f26aa02f90284a
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-16 10:02:20 -0700

    creat determine cost function inside Pizza object

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Describe: Pizza()
 Test: "It should return a Pizza object with two properties for toppings and size"
 Code: const myPizza = new Pizza("medium", ["anchovies", "pineapple"]);
 Expected Output: Pizza { toppings: size: "medium" ["anchovies", "pineapple"], }
+
+Describe: Pizza.prototype.cost();
+
+Test: "It should return a Pizza object with one property for cost depending on the inputs for toppings and size"
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,34 @@
 //UI logic
 //in order to recreate an instance of Pizza
-let myPizza = new Pizza();
+let myPizza = new Pizza("small", ["cheese", "pepp", "moose"]);
 
 //Buisness logic
 
 //adding Pizza constructor:
-function Pizza(size, toppings);
+function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
+  this.price = 0;
+}
 
+//function to determine cost based off properties of Pizza
+Pizza.prototype.determineCost = function(){
+  let sizeFactor = 0;
+  let toppingFactor = 0;
+  if (this.size = "small") {
+    sizeFactor = 1;
+  } else if (this.size = "medium") {
+    sizeFactor = 1.5;
+  } else if (this.size = "large") {
+    sizeFactor = 1.75;
+  }
+
+  if (this.toppings.length < 3) {
+    toppingFactor = 1;
+  } else if (this.toppings.length >= 3 && this.toppings.length <= 4) {
+    toppingFactor = 3;
+  } else if (this.toppings.length > 4) {
+    toppingFactor = 5;
+  }
+  return 10 * sizeFactor + toppingFactor;
+}
